f1a2fc538680a37b03821dac43c8e39b39d711fc
Ishika-07 <60807873+Ishika-07@users.noreply.github.com>
2021-12-09 09:51:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from "./screens/Main";
+
+export default function App() {
+  return (
+    <Main/>
+  );
+}
+
